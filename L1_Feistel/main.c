b6684f331360eb3fb9961829d963d5845ec99e59
Artyom Vakulin <vakulin95@outlook.com>
2017-04-08 18:14:32 +0300

    [L1] - Some defines added

--- L1_Feistel/main.c
+++ L1_Feistel/main.c
@@ -2,9 +2,14 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define DEF_ROUNDS 250
-#define DEF_MES_LEN 64
-#define GET_BIT(N, K) ((N & ((long long int)1 << K)) >> K)
+#define DEF_ROUNDS          10
+#define DEF_MES_LEN         64
+
+#define GET_BIT(X, NUM)         (( (X) & ( (int64)(1) << (NUM) )) >> (NUM) )
+#define GET_PART(X, NUM)        ((int16)( (X) >> (NUM) * 16))
+#define SET_PART(X, PART, NUM)  (((int64)(PART) << 16 * (NUM)) | (int64)(X) )
+#define MOOVE_R_B(X, Y)         (( (X) >> (Y) ) | ( (X) << ( (Y) & (int32)(15) )))
+#define MOOVE_L_B(X, Y)         (( (X) << (Y) ) | ( (X) >> ( (Y) & (int32)(15) )))
 
 typedef unsigned short int      int16;
 typedef unsigned int            int32;
@@ -13,19 +18,17 @@ typedef unsigned long long int  int64;
 int16 KEYS[DEF_ROUNDS];
 char BIN[DEF_MES_LEN];
 
-int16 RotLeft16(int16 n, int32 c);
-int16 RotRight16(int16 n, int32 c);
 int GetKeys(void);
-int16 GetBlock(int64 num, int32 block);
 int64 Merge(int16 b0, int16 b1, int16 b2, int16 b3);
 int32 EncryptFunction(int16 num0, int16 num1, int16 num2, int16 key);
 int64 Encode(int64 num, int keysCount);
 int64 Decode(int64 num, int keysCount);
 void bits_to_str(int64 N);
+void print_b(char str[255], int N);
 
 int main()
 {
-    unsigned long long int a = 880035355599669969;
+    int64 a = 880035355599669969;
 
     bits_to_str(a);
     printf("Input: \t\t%.*s\n", DEF_MES_LEN, BIN);
@@ -43,20 +46,6 @@ int main()
     return 0;
 }
 
-int16 RotLeft16(int16 n, int32 c)
-{
-	const unsigned int mask = (8*sizeof(n) - 1);
-	c &= mask;
-	return (n << c) | (n >> ((c)&mask));
-}
-
-int16 RotRight16(int16 n, int32 c)
-{
-	const unsigned int mask = (8*sizeof(n) - 1);
-	c &= mask;
-	return (n >> c) | (n << ((c)&mask));
-}
-
 int GetKeys(void)
 {
     int i;
@@ -65,25 +54,28 @@ int GetKeys(void)
 
 	for(i = 0; i < DEF_ROUNDS - 1; i++)
 	{
-		KEYS[i] = RotRight16((int16)rand(), i * 8);
+        KEYS[i] = MOOVE_R_B((int16)rand(), i * 8);
 	}
 
 	return 0;
 }
 
-int16 GetBlock(int64 num, int32 block)
-{
-	return (int16)(num >> block * 16);
-}
-
 int64 Merge(int16 b0, int16 b1, int16 b2, int16 b3)
 {
+    int i;
+    int64 Y = 0;
+
+    // for(i = 0; i < 4; i++)
+    // {
+    //     SET_PART(Y, )
+    // }
+
 	return (int64)b0 << 16 * 0 | (int64)b1 << 16 * 1 | (int64)b2 << 16 * 2 | (int64)b3 << 16 * 3;
 }
 
 int32 EncryptFunction(int16 num0, int16 num1, int16 num2, int16 key)
 {
-	return RotLeft16(num0, 9) ^ RotLeft16(num1, 9) ^ RotLeft16(num2, 9) ^ (~(RotRight16(key, 11) ^ num0 ^ num1 ^ num2));
+	return MOOVE_L_B(num0, 9) ^ MOOVE_L_B(num1, 9) ^ MOOVE_L_B(num2, 9) ^ (~(MOOVE_R_B(key, 11) ^ num0 ^ num1 ^ num2));
 }
 
 int64 Encode(int64 num, int keysCount)
@@ -93,10 +85,10 @@ int64 Encode(int64 num, int keysCount)
 
 	GetKeys();
 
-	int16 b0 = GetBlock(num, 0);
-	int16 b1 = GetBlock(num, 1);
-	int16 b2 = GetBlock(num, 2);
-	int16 b3 = GetBlock(num, 3);
+	int16 b0 = GET_PART(num, 0);
+	int16 b1 = GET_PART(num, 1);
+	int16 b2 = GET_PART(num, 2);
+	int16 b3 = GET_PART(num, 3);
 
 	for (i = 0; i < DEF_ROUNDS; i++)
 	{
@@ -125,10 +117,10 @@ int64 Decode(int64 num, int keysCount)
 
 	GetKeys();
 
-	int16 b0 = GetBlock(num, 0);
-	int16 b1 = GetBlock(num, 1);
-	int16 b2 = GetBlock(num, 2);
-	int16 b3 = GetBlock(num, 3);
+	int16 b0 = GET_PART(num, 0);
+	int16 b1 = GET_PART(num, 1);
+	int16 b2 = GET_PART(num, 2);
+	int16 b3 = GET_PART(num, 3);
 
 	for (i = DEF_ROUNDS - 1; i >= 0; i--)
 	{
@@ -159,6 +151,20 @@ void bits_to_str(int64 N)
     {
         BIN[DEF_MES_LEN - i - 1] = GET_BIT(N, i) + '0';
     }
+
+    return;
+}
+
+void print_b(char str[255], int N)
+{
+    int i;
+    printf("%s", str);
+
+    for(i = sizeof(N) * 8 - 1; i >= 0; i--)
+    {
+        printf("%c", GET_BIT(N, i) + '0');
+    }
+    printf("\n");
 
     return;
 }
