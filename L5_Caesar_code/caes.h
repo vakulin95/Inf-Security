3c1d7ec46a4bd5cbadd76023d451491534084b2b
Artyom Vakulin <vakulin95@outlook.com>
2017-04-27 23:22:36 +0300

    [L5] - INIT

---
+++ L5_Caesar_code/caes.h
@@ -0,0 +1,19 @@
+#ifndef __CAESAR_H__
+#define __CAESAR_H__
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <float.h>
+#include <gmp.h>
+
+
+//---------------------------------------------------------------------------//
+
+
+//---------------------------------------------------------------------------//
+
+
+#endif
