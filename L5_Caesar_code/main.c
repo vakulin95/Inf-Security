7f90a4e620115c3abf86747577168b090cd60cbc
Artyom Vakulin <vakulin95@outlook.com>
2017-04-28 13:11:11 +0300

    [L5] - wchar_t type using

--- L5_Caesar_code/main.c
+++ L5_Caesar_code/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <locale.h>
 #include "caes.h"
 
 int main(int argc, char *argv[])
 {
+    setlocale(LC_ALL, "");
+
     printf("\n-----------------------------------------Caesar's code-----------------------------------------\n\n");
+
+    printf("String Entered:\n\n%ls\n\nlength: %d\n", TEXT, wcslen(TEXT));
+
+    // if(TEXT[0] == L'w')
+    // {
+    //     printf("+\n");
+    // }
+    // else
+    // {
+    //     printf("-\n");
+    // }
+
     printf("-----------------------------------------------------------------------------------------------\n");
 
     return 0;
