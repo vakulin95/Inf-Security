a0693b0e241617271847c9efc9e9a37bdf567593
Artyom Vakulin <vakulin95@outlook.com>
2017-10-18 19:00:57 +0300

    [L6] - compress and decompress working correctly

--- L6_Stegosclusion/main.c
+++ L6_Stegosclusion/main.c
@@ -9,150 +9,75 @@
 #define DEF_IM_WIDTH        400
 #define DEF_IM_HEIGHT       400
 #define DEF_IM_QUALITY      75
+#define DEF_IM_DIM          3
 
-unsigned char T[DEF_IM_WIDTH][DEF_IM_HEIGHT][3];
+unsigned char T[DEF_IM_WIDTH][DEF_IM_HEIGHT][DEF_IM_DIM];
 
-typedef struct jpeg_decompress_struct JPG_DEC;
-
-int read_jpg(unsigned char **pBuf);
-int load_im_meta(int *w, int *h, int *n);
-void load_im_mass(unsigned char **im);
-void write_jpg(unsigned char *im, char *filename);
+int load_im_mass(char *filename);
+void write_jpg(char *filename);
 
 int main(void)
 {
-    int im_width, im_height, im_comp;
-    unsigned char *im;
+    size_t i, j;
 
-    im = (unsigned char *)malloc(load_im_meta(&im_width, &im_height, &im_comp));
-    printf("%d %d %d\n", im_width, im_height, im_comp);
-    load_im_mass(&im);
-
-
-
-    write_jpg(im, "out.jpg");
+    load_im_mass("in1.jpg");
+    write_jpg("out.jpg");
 
     return 0;
 }
 
-int read_jpg(unsigned char **im)
+int load_im_mass(char  *filename)
 {
     size_t i, j;
-    struct jpeg_decompress_struct d1;
-    struct jpeg_error_mgr m1;
-
-    d1.err = jpeg_std_error (&m1);
-    jpeg_create_decompress(&d1);
-    FILE *f = fopen("files/in.jpg","rb");
-    jpeg_stdio_src(&d1, f);
-    jpeg_read_header(&d1, TRUE);
-
-    int w,h;
-    int n;
-    w = d1.image_width;
-    h = d1.image_height;
-    n = d1.num_components;
-    printf ("Passed %d x %d x %d.\n", w, h, n);
-
-    int n_s = w * n; // RGB, num. of the samples
     unsigned char *pBuf;
-    unsigned char aR,aG,aB;
-    unsigned long sR = 0;
-    unsigned long sG = 0;
-    unsigned long sB = 0;
+    char path[DEF_PATH_STR_LEN];
 
-    pBuf = (unsigned char *)malloc(n_s * h);
-    jpeg_start_decompress(&d1);
-    i = 0;
-    while (d1.output_scanline < d1.output_height)
+    struct jpeg_decompress_struct Y;
+    struct jpeg_error_mgr m1;
+
+    Y.err = jpeg_std_error (&m1);
+    jpeg_create_decompress(&Y);
+
+    sprintf(path, "%s%s", DEF_PATH, filename);
+
+    FILE *f = fopen(path,"rb");
+
+    jpeg_stdio_src(&Y, f);
+    jpeg_read_header(&Y, TRUE);
+
+    if(Y.image_width != DEF_IM_WIDTH || Y.image_height != DEF_IM_HEIGHT || Y.num_components != DEF_IM_DIM)
     {
-        // Получить экранную строку
-        i += jpeg_read_scanlines(&d1,(JSAMPARRAY)&(pBuf),1);
-        // for (j = 0; j < w; j++)
-        // {
-        //     sR += pBuf[j*n + 0];
-        //     sG += pBuf[j*n + 1];
-        //     sB += pBuf[j*n + 2];
-        // }
+        printf("load_im_mass(): Image size!\n");
+        getchar();
+        return 1;
     }
-    // aR = sR/(w*h);
-    // aG = sG/(w*h);
-    // aB = sB/(w*h);
-    //
-    // printf("%02X %02X %02X\n",aR,aG,aB);
-    jpeg_finish_decompress(&d1);
-    jpeg_destroy_decompress(&d1);
-
-    fclose(f);
-
-    *im = pBuf;
-    free(pBuf);
-
-    return 0;
-}
-
-int load_im_meta(int *w, int *h, int *n)
-{
-    int retval;
-    JPG_DEC Y;
-    struct jpeg_error_mgr m1;
-
-    Y.err = jpeg_std_error (&m1);
-    jpeg_create_decompress(&Y);
-    FILE *f = fopen("files/in.jpg","rb");
-    jpeg_stdio_src(&Y, f);
-    jpeg_read_header(&Y, TRUE);
-
-    *w = Y.image_width;
-    *h = Y.image_height;
-    *n = Y.num_components;
-
-    retval = (*w) * (*h) * (*n);
-
-    jpeg_destroy_decompress(&Y);
-
-    fclose(f);
-
-    return retval;
-}
-
-void load_im_mass(unsigned char **im)
-{
-    JPG_DEC Y;
-    size_t i, j, k;
-
-    struct jpeg_error_mgr m1;
-
-    Y.err = jpeg_std_error (&m1);
-    jpeg_create_decompress(&Y);
-
-    FILE *f = fopen("files/in.jpg","rb");
-
-    jpeg_stdio_src(&Y, f);
-    jpeg_read_header(&Y, TRUE);
 
     jpeg_start_decompress(&Y);
-    j = 0;
-    while (Y.output_scanline < Y.output_height)
+    i = 0;
+    pBuf = (unsigned char *)malloc(DEF_IM_HEIGHT * DEF_IM_WIDTH * DEF_IM_DIM);
+    for(i = 0; i < DEF_IM_HEIGHT; i++)
     {
         // Получить экранную строку
-        jpeg_read_scanlines(&Y, (JSAMPARRAY)im, 1);
-        for (i = 0; i < Y.image_width; i++)
+        jpeg_read_scanlines(&Y, (JSAMPARRAY)&(pBuf), 1);
+        for (j = 0; j < DEF_IM_WIDTH; j++)
         {
-            T[j][i][0] = (*im)[i*Y.num_components + 0];
-            T[j][i][1] = (*im)[i*Y.num_components + 1];
-            T[j][i][2] = (*im)[i*Y.num_components + 2];
+            T[i][j][0] = pBuf[j*Y.num_components + 0];
+            T[i][j][1] = pBuf[j*Y.num_components + 1];
+            T[i][j][2] = pBuf[j*Y.num_components + 2];
         }
-        j++;
+        // i++;
     }
 
     jpeg_finish_decompress(&Y);
     jpeg_destroy_decompress(&Y);
+    free(pBuf);
 
     fclose(f);
+
+    return 0;
 }
 
-void write_jpg(unsigned char *scr, char *filename)
+void write_jpg(char *filename)
 {
     FILE *out;
     char path[DEF_PATH_STR_LEN];
@@ -172,7 +97,7 @@ void write_jpg(unsigned char *scr, char *filename)
 
     cinfo.image_width = DEF_IM_WIDTH;
     cinfo.image_height = DEF_IM_HEIGHT;
-    cinfo.input_components = 3;
+    cinfo.input_components = DEF_IM_DIM;
     cinfo.in_color_space = JCS_RGB;
 
     jpeg_set_defaults(&cinfo);
@@ -181,12 +106,11 @@ void write_jpg(unsigned char *scr, char *filename)
 
     JSAMPROW row_pointer[1];
     int row_stride;
-    row_stride = cinfo.image_width*3;
+    row_stride = cinfo.image_width*DEF_IM_DIM;
 
     i = 0;
     while (cinfo.next_scanline < cinfo.image_height)
     {
-        // row_pointer[0] = (JSAMPLE *)(scr+cinfo.next_scanline*row_stride);
         row_pointer[0] = (JSAMPLE *)(T[i]);
         jpeg_write_scanlines(&cinfo, row_pointer, 1);
         i++;
@@ -194,6 +118,4 @@ void write_jpg(unsigned char *scr, char *filename)
 
     jpeg_finish_compress(&cinfo);
     jpeg_destroy_compress(&cinfo);
-
-    free(scr);
 }
